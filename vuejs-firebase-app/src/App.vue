098f1ef94ffa09c2cb22e59823e00ff1ba6250f6
gearmobile <gearmobile@gmail.com>
2017-04-06 23:11:23 +0300

    vuejs-firebase

---
+++ vuejs-firebase-app/src/App.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  #app
+</template>
+
+<script>
+  import Firebase from 'firebase'
+  const config = {
+    apiKey: 'AIzaSyCCtn-Y-8cEfTjHMA-rkdbrKfQULQ4d0zE',
+    authDomain: 'vue-firebase-a56dd.firebaseapp.com',
+    databaseURL: 'https://vue-firebase-a56dd.firebaseio.com',
+    projectId: 'vue-firebase-a56dd',
+    storageBucket: 'vue-firebase-a56dd.appspot.com',
+    messagingSenderId: '747876873568'
+  }
+  const app = Firebase.initializeApp(config)
+  const db = app.database()
+  console.log(db)
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
