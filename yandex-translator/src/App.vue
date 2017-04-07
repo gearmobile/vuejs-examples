b1fbecba08bc2ca595626a19c767bf5ce762e7d8
gearmobile <gearmobile@gmail.com>
2017-04-07 11:26:54 +0300

    vuejs-yandex

---
+++ yandex-translator/src/App.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  #app.translator
+    .container.translator__container
+      .row.translator__row
+        .col-md-8.col-md-offset-2.translator__column
+          h1.page-header.text-center.translator__title {{ title }}
+          h4.page-header.text-center.translator__subtitle {{ subtitle }}
+</template>
+
+<script>
+  import inputTranslate from './components/inputTranslate.vue'
+  import outputTranslate from './components/outputTranslate.vue'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        title: 'yandex word translator',
+        subtitle: 'Powered vy Vue.js and Booswatch'
+      }
+    },
+    components: {
+      appInputTranslate: inputTranslate,
+      appOutputTranslate: outputTranslate
+    }
+  }
+</script>
+
+<style lang="scss">
+  .translator {
+
+    &__title {
+      text-transform: capitalize;
+      font-weight: 700;
+      font-size: 30px;
+      color: forestgreen;
+      margin-bottom: 0;
+    }
+
+    &__subtitle {
+      margin-top: 0;
+      font-size: 15px;
+      color: rgba( 0,0,0, .6 )
+    }
+  }
+</style>
---
+++ yandex-translator/src/components/inputTranslate.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .input-group
+    span.input-group-btn
+      button.btn.btn-default( type="button" ) translate!
+    input.form-control( type="text", placeholder="Input word to translate" )
+</template>
+
+<script>
+  export default {
+    name: 'input',
+    data () {
+      return {
+        // msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  //
+</style>
