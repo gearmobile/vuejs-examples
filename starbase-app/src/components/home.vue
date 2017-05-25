e89944eb4bfb19f8304290c6ff38c7a2effe6875
gearmobile <gearmobile@gmail.com>
2017-05-25 09:45:13 +0300

    starbase

--- starbase-app/src/components/home.vue
+++ starbase-app/src/components/home.vue
@@ -1,36 +1,55 @@
 <template lang="pug">
   #home
     h1.display-4.text-capitalize generate your team
-    button.btn.btn-primary( type="button", @click="onFetchData('people')" ) fetch data
+    button.btn.btn-primary( type="button", @click="fetchPersons(3)" ) fetch data
     .row
       .col-sm-6
         .card-group
-          app-card( v-for="(card, index) in cards", :key="index", :character="card" )
+          app-card( v-for="(person, index) in persons", :key="index", :character="person" )
 </template>
 
 <script>
   import Card from '../components/card.vue'
   import axios from 'axios'
-  const baseURL = 'http://swapi.co/api/'
+  const baseURL = 'http://swapi.co/api/people/'
 
   export default {
     name: 'home',
     data () {
       return {
-        cards: []
+        quantity: 6,
+        count: null,
+        ids: [],
+        persons: []
       }
     },
     methods: {
-      onFetchData (value) {
-        axios.get(baseURL + value + '/')
+      counter () {
+        axios.get(baseURL)
           .then(response => {
-            console.log(response.data)
-            this.cards = response.data.results
+            this.count = parseInt(response.data.count)
           })
           .catch(error => {
             console.log(error)
           })
+      },
+      randomID () {
+        return Math.floor(Math.random() * this.count) + 1
+      },
+      fetchPersons (quantity) {
+        for (let i = 0; i < quantity; i += 1) {
+          axios.get(baseURL + this.randomID())
+            .then(response => {
+              this.persons.push(response.data)
+            })
+            .catch(error => {
+              console.log(error)
+            })
+        }
       }
+    },
+    created () {
+      this.counter()
     },
     components: {
       appCard: Card
@@ -39,5 +58,7 @@
 </script>
 
 <style scoped>
-  /**/
+  button {
+    cursor: pointer;
+  }
 </style>
