af269c98b74e57ec8d6308784b88728c99e65bf3
gearmobile <gearmobile@gmail.com>
2017-05-25 14:56:34 +0300

    starbase

--- starbase-app/src/components/card.vue
+++ starbase-app/src/components/card.vue
@@ -2,11 +2,10 @@
   .card.card-outline-info( @click="onClick()" )
     .card-block
       h4.card-title {{ character.name }}
-      p.card-text
-        | This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-      p.card-text
-        small.text-muted Last updated 3 mins ago
-    //- .overlay( :mousemove="onOver()" )
+      p.card-text #[strong Height:] {{ character.height }}cm
+      p.card-text #[strong Gender:] {{ character.gender }}
+      p.card-text #[strong Hair Color:] {{ character.hair_color }}
+      p.card-text #[strong Eye Color:] {{ character.eye_color }}
 </template>
 
 <script>
@@ -18,34 +17,20 @@
         default: null
       }
     },
-    data () {
-      return {
-        //
-      }
-    },
     methods: {
       onClick () {
         this.$emit('onSelect', this.character)
-      },
-      onOver () {
-        console.log('over')
       }
     }
   }
 </script>
 
 <style scoped>
-  .overlay {
-    position: absolute;
-    /*display: none;*/
-    width: 100%;
-    height: 100%;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-color: rgba( 0,0,0, .5 );
-    z-index: 2;
+  .card:hover {
+    background-color: rgba( 0,0,0, .2 );
     cursor: pointer;
+  }
+  .card-block .card-text {
+    margin-bottom: .2rem;
   }
 </style>
