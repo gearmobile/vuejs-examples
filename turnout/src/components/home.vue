c29ab329e94f31bc77b295d9fee02e2f8b86ccce
gearmobile <gearmobile@gmail.com>
2017-05-26 15:52:03 +0300

    turnout

---
+++ turnout/src/components/home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  #home
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      // msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  //
+</style>
