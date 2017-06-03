e2eee90cf86d5ff0580a439ca68960e63a316ccf
gearmobile <gearmobile@gmail.com>
2017-06-03 19:52:36 +0300

    window-app

--- window-calc-app/src/components/single-window.vue
+++ window-calc-app/src/components/single-window.vue
@@ -5,7 +5,7 @@
       form.well
         .form-group
           label( for="height" ) Высота
-          input.form-control( type="text", id="height" )
+          input.form-control( type="text", id="height", v-model="valueHeight" )
         .form-group
           label( for="width" ) Ширина
           input.form-control( type="text", id="width", v-model="valueWidth" )
@@ -26,28 +26,36 @@
               | Откосы
     .col-md-8
       h3 Тип открывания окна
-      .well
-        vue-slider( ref="sliderHor", v-model="valueWidth" )
-        //- input( type="range", :min="singleHorMin", :max="singleHorMax", v-model="width", step="10" )
+      .well( style="height: 600px" )
+        vue-ver-slider.vertical-slider( ref="sliderVer", direction="vertical", height="300", width="3", min="1000", max="1650", interval="10", v-model="valueHeight" )
+        vue-hor-slider.horizontal-slider( ref="sliderHor", min="400", max="1400", interval="10", height="3", width="90%", v-model="valueWidth" )
 </template>
 
 <script>
   import vueSlider from 'vue-slider-component'
+
   export default {
     name: 'single-window',
     data () {
       return {
-        valueWidth: 0,
-        tooltip: false,
-        direction: 'vertical'
+        valueWidth: 1400,
+        valueHeight: 1650
       }
     },
     components: {
-      vueSlider
+      vueHorSlider: vueSlider,
+      vueVerSlider: vueSlider
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  //
+  .vertical-slider {
+    display: inline-block;
+    height: 300px;
+    margin-left: 1%;
+  }
+  .horizontal-slider {
+    margin-left: 5%;
+  }
 </style>
