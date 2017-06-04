1d9ae99169590d7775d3126e582eaae541c0ddad
gearmobile <gearmobile@gmail.com>
2017-06-04 12:46:39 +0300

    add windows-calc with quasar

---
+++ calc-window/src/components/Index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  q-layout
+
+    // tabs
+    q-tabs( slot="navigation" )
+      q-tab( icon="home", route="/" ) Home
+
+
+    // page content
+    .layout-view
+      .layout-padding
+      
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        //
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  //
+</style>
