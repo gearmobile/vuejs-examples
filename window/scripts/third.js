e1b6584763cc17f8ec94d0df7ba13683ca81768d
gearmobile <gearmobile@gmail.com>
2017-06-10 09:55:45 +0300

    window

--- window/scripts/third.js
+++ window/scripts/third.js
@@ -2,8 +2,8 @@ const app = new Vue({
   data: {
     window: {
       dimentions: {
-        width: null,
-        height: null
+        width: 1000,
+        height: 1000
       },
       profile: {
         price: 1000,
@@ -21,13 +21,13 @@ const app = new Vue({
       checked: [],
       additions: {
         montage: {
-          price: 1000
+          price: 386
         },
         slope: {
-          price: 1000
+          price: 193
         },
         sill: {
-          price: 1000
+          price: 1600
         }
       }
     },
@@ -53,25 +53,71 @@ const app = new Vue({
       cost: {
         window: null,
         work: null,
-        total: null
+        total: null,
+        additional: null
       }
     }
   },
+  filters: {
+    singleDivider (value) {
+      return value / 1000
+    },
+    doubleDivider (value) {
+      return value / 1000000
+    },
+    roundDown (value) {
+      return Math.round(value) // TODO добавить разделитель тысячных долей
+    }
+  },
+  computed: {
+    imagePath () {
+      let path = null
+      switch (this.window.type) {
+        case 'simple':
+          path = './images/doggy-01.jpg'
+          break
+        case 'turn':
+          path = './images/doggy-02.jpg'
+          break
+        case 'tilt':
+          path = './images/doggy-04.jpg'
+          break
+        default:
+          path = './images/doggy-04.jpg'
+      }
+      return path
+    },
+    // CALC MONTAGE COST
+    montageCost () {
+      return (this.profileOuterLength(this.window.dimentions.height, this.window.dimentions.width) / 1000) * this.window.additions.montage.price
+    },
+    sillCost () {
+      return (this.window.dimentions.width / 1000) * this.window.additions.sill.price
+    },
+    // CALC SLOP COST
+    slopeCost () {
+      return (this.profileQuater(this.window.dimentions.height, this.window.dimentions.width) / 1000) * this.window.additions.slope.price
+    }
+  },
   methods: {
+    profileQuater (height, width) {
+      const totalLength = parseInt(height) * 2 + parseInt(width)
+      return totalLength
+    },
     profileOuterLength (height, width) {
-      const totalLength = ((parseInt(width) + parseInt(height)) * 2) / 1000
+      const totalLength = (parseInt(width) + parseInt(height)) * 2
       return totalLength
     },
     profileInnerLength (height, width, profile) {
       const innerWidth = parseInt(width) - parseInt(profile) * 2
       const innerHeight = parseInt(width) - parseInt(profile) * 2
-      const totalLength = ((innerWidth + innerHeight) * 2) / 1000
+      const totalLength = (innerWidth + innerHeight) * 2
       return totalLength
     },
     packetArea (height, width, profile) {
       const packetHeight = parseInt(height) - parseInt(profile) * 2
       const packetWidth = parseInt(width) - parseInt(profile) * 2
-      const packetArea = (packetWidth * packetHeight) / 1000000
+      const packetArea = packetWidth * packetHeight
       return packetArea
     },
     productCost (profileLength, packetArea, profileCost, packetCost) {
@@ -84,8 +130,8 @@ const app = new Vue({
       const costWork = (parseInt(totalSum) * parseInt(percent)) / 100
       return costWork
     },
-    commonCost (costProduct, costWork) {
-      const costCommon = parseInt(costProduct) + parseInt(costWork)
+    commonCost (costProduct, costWork, costAddittions) {
+      const costCommon = parseInt(costProduct) + parseInt(costWork) + parseInt(costAddittions)
       return costCommon
     },
     clearForm () {
@@ -95,9 +141,10 @@ const app = new Vue({
     simpleWindow () {
       this.output.profile.total = this.profileOuterLength(this.window.dimentions.height, this.window.dimentions.width)
       this.output.area = this.packetArea(this.window.dimentions.height, this.window.dimentions.width, this.window.profile.thickness)
-      this.output.cost.window = this.productCost(this.output.profile, this.output.area, this.window.profile.price, this.window.packet.price)
+      // price calculate
+      this.output.cost.window = this.productCost(this.output.profile.total, this.output.area, this.window.profile.price, this.window.packet.price)
       this.output.cost.work = this.workCost(this.output.cost.window, this.window.percent)
-      this.output.cost.total = this.commonCost(this.output.cost.window, this.output.cost.window)
+      this.output.cost.total = this.commonCost(this.output.cost.window, this.output.cost.work, this.output.cost.additional)
       this.clearForm()
     },
     secondWindow () {
@@ -120,11 +167,21 @@ const app = new Vue({
         this.output.cost.total = this.commonCost(this.output.cost.window, this.output.cost.work) + this.window.furnitura.titl
         this.clearForm()
     },
-    onCalc () {
-      if (!this.window.dimentions.width && !this.window.dimentions.height) {
-        alert('error')
-        return
+    checkAdditions () {
+      const arr = this.window.checked
+      for (let i = 0; i < arr.length; i += 1) {
+        if (arr[i] === 'montage') {
+          this.output.cost.additional += parseInt(this.window.additions.montage.price)
+        }
+        if (arr[i] === 'sill') {
+          this.output.cost.additional += parseInt(this.window.additions.sill.price)
+        }
+        if (arr[i] === 'slope') {
+          this.output.cost.additional += parseInt(this.window.additions.slope.price)
+        }
       }
+    },
+    selectCalc () {
       switch (this.window.type) {
         case 'simple':
           this.simpleWindow()
@@ -138,6 +195,16 @@ const app = new Vue({
         default:
           break
       }
+    },
+    onCalc () {
+      // check if fields are empty
+      if (!this.window.dimentions.width || !this.window.dimentions.height) {
+        alert('error')
+        return
+      }
+      // if fields are not empty
+      this.checkAdditions()
+      this.selectCalc()
     }
   }
 });
