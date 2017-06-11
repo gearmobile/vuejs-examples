513627153dff119d93e2de9681eefa9d8216bad6
gearmobile <gearmobile@gmail.com>
2017-06-11 22:29:28 +0300

    window-app

--- window-app/src/components/singleWindow.vue
+++ window-app/src/components/singleWindow.vue
@@ -1,18 +1,78 @@
 <template lang="pug">
-  #single
+  .single-window
+
+    // SET WINDOW DIMENTIONS
     h2.page-header Одинарное окно
     section.well
       h3.page-header Размеры окна:
       .form-group
-        label
+        label.single-window__label
           | Window Height: 
           span {{ heightWindow }}мм
-          input(:min='minH', :max='maxH', :step='stepH', type='range', v-model='height')
+          input.single-window__input(:min='minH', :max='maxH', :step='stepH', type='range', v-model='height')
       .form-group
-        label
+        label.single-window__label
           | Window Width: 
           span {{ widthWindow }}мм
-          input(:min='minW', :max='maxW', :step='stepW', type='range', v-model='width')
+          input.single-window__input(:min='minW', :max='maxW', :step='stepW', type='range', v-model='width')
+
+    // SET TYPE WINDOW
+    section.well
+      h3.page-header Тип окна:
+      .row
+        .col-md-6
+          h4.page-header Выберите тип окна:
+          .radio(v-for='(type, index) in types')
+            label(:for='type.title')
+              input(:id='type.title', name='type', type='radio', :value='type.title', v-model='window')
+              |  {{ type.name }}
+        .col-md-6
+          h4.page-header Предпросмотр типа окна:
+          figure.picture
+            img.picture__inner(:src='previewWindow', alt='Window Preview')
+
+    // PROFILES
+    section.form-group.well
+      h3.page-header Профиль:
+      .radio
+        label.item.list-group-item.text-capitalize(v-for='(profile, index) in profiles', :key='index')
+          input(type='radio', :value='profile.title', name='check-profile', v-model='profileType')
+          header.item__wrapper(@click='profile.show = !profile.show')
+            h4.item__title.text-capitalize {{ profile.title }}
+            h3.item__label {{ profile.details.type }}
+          transition
+            footer.item__content.bg-info(v-if='profile.show')
+              h4.item__subtitle {{ profile.details.type }}
+              .row
+                .col-xs-6
+                  ul
+                    li Стеклопакет: {{ profile.details.packet }}
+                    li Формула: {{ profile.details.formula }}
+                    li Фурнитура: {{ profile.details.furnitura }}
+                    li Базовый цвет: {{ profile.details.color }}
+                    li Уплотнитель: {{ profile.details.sealer }}
+                .col-xs-6
+                  figure
+                    img.img-responsive.img-thumbnail(:src='profile.preview', :alt='profile.title')
+
+    // ADDITIONS
+    section.well
+      h3.page-header Дополнительно:
+      .checkbox
+        label.checkbox-inline(for='uno')
+          input#uno(type='checkbox', value='montage', v-model='window.checked')
+          |  Монтаж
+          p.text-muted +{{ montageCost | roundDown }} руб
+      .checkbox
+        label.checkbox-inline(for='duo')
+          input#duo(type='checkbox', value='sill', v-model='window.checked')
+          |  Подоконник
+          p.text-muted +{{ sillCost | roundDown }} руб
+      .checkbox
+        label.checkbox-inline(for='tre')
+          input#tre(type='checkbox', value='slope', v-model='window.checked')
+          |  Откосы
+          p.text-muted +{{ slopeCost | roundDown }} руб
 
 
 </template>
@@ -22,11 +82,6 @@
 
   export default {
     name: 'singleWindow',
-    data () {
-      return {
-        // msg: 'single window'
-      }
-    },
     computed: {
       ...mapGetters({
         heightWindow: 'windowHeight',
@@ -36,7 +91,10 @@
         stepH: 'stepHeight',
         minW: 'minWidth',
         maxW: 'maxWidth',
-        stepW: 'stepWidth'
+        stepW: 'stepWidth',
+        types: 'windowTypes',
+        previewWindow: 'windowPreview',
+        profiles: 'getProfiles'
       }),
       height: {
         get () {
@@ -53,6 +111,22 @@
         set (value) {
           this.$store.dispatch('updateWidth', value)
         }
+      },
+      window: {
+        get () {
+          return this.$store.state.checked.window
+        },
+        set (value) {
+          this.$store.dispatch('updateWindowType', value)
+        }
+      },
+      profileType: {
+        get () {
+          return this.$store.state.checked.profile
+        },
+        set (value) {
+          this.$store.dispatch('updateWindowProfile', value)
+        }
       }
     },
     methods: {
@@ -65,5 +139,16 @@
 </script>
 
 <style lang="scss" scoped>
-  //
+  .single-window {
+
+    &__label {
+      width: 100%;
+    }
+
+    &__input {
+      margin-top: 15px;
+      width: 80%;
+      margin-left: 10%;
+    }
+  }
 </style>
--- window-app/src/store/index.js
+++ window-app/src/store/index.js
@@ -4,6 +4,10 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
+  checked: {
+    window: 'tilt',
+    profile: 'Стандарт'
+  },
   singleWindow: {
     dimentions: {
       minHeight: 1000,
@@ -14,10 +18,98 @@ const state = {
       stepWidth: 10
     },
     sizes: {
-      height: 1000,
+      height: 1200,
       width: 1000
     }
-  }
+  },
+  types: [
+    {
+      title: 'simple',
+      name: 'Глухое окно'
+    },
+    {
+      title: 'turn',
+      name: 'Поворотное окно'
+    },
+    {
+      title: 'tilt',
+      name: 'Поворотно-откидное окно'
+    }
+  ],
+  profiles: [
+    {
+      title: 'Стандарт',
+      details: {
+        type: 'Rehau Blitz',
+        packet: '32мм, 2 камеры',
+        formula: '4x10x4x10x4',
+        furnitura: 'Siegenia-Aubi, Favorit',
+        color: 'белый',
+        sealer: 'черный'
+      },
+      price: {
+        profile: 1000,
+        furnitura: 500,
+        packet: 500
+      },
+      preview: './assets/doggy-01.jpg',
+      show: true
+    },
+    {
+      title: 'Комфорт',
+      details: {
+        type: 'Rehau Delight',
+        packet: '32мм, 2 камеры',
+        formula: '4x10x4x10x4',
+        furnitura: 'Siegenia-Aubi, Favorit',
+        color: 'белый',
+        sealer: 'черный'
+      },
+      price: {
+        profile: 1100,
+        furnitura: 600,
+        packet: 600
+      },
+      preview: './assets/doggy-02.jpg',
+      show: false
+    },
+    {
+      title: 'Премиум',
+      details: {
+        type: 'Rehau Brilliant',
+        packet: '32мм, 2 камеры',
+        formula: '4x10x4x10x4',
+        furnitura: 'Siegenia-Aubi, Favorit',
+        color: 'белый',
+        sealer: 'черный'
+      },
+      price: {
+        profile: 1200,
+        furnitura: 700,
+        packet: 700
+      },
+      preview: './assets/doggy-04.jpg',
+      show: false
+    },
+    {
+      title: 'Эксклюзив',
+      details: {
+        type: 'Rehau Geneo',
+        packet: '32мм, 2 камеры',
+        formula: '4x10x4x10x4',
+        furnitura: 'Siegenia-Aubi, Favorit',
+        color: 'белый',
+        sealer: 'черный'
+      },
+      price: {
+        profile: 1300,
+        furnitura: 800,
+        packet: 800
+      },
+      preview: './assets/doggy-05.jpg',
+      show: false
+    }
+  ]
 }
 
 const getters = {
@@ -44,6 +136,29 @@ const getters = {
   },
   windowWidth (state) {
     return state.singleWindow.sizes.width
+  },
+  windowTypes (state) {
+    return state.types
+  },
+  windowPreview (state) {
+    let path = null
+    switch (state.checked.window) {
+      case 'simple':
+        path = './assets/doggy-01.jpg'
+        break
+      case 'turn':
+        path = './assets/doggy-02.jpg'
+        break
+      case 'tilt':
+        path = './assets/doggy-04.jpg'
+        break
+      default:
+        path = './assets/doggy-04.jpg'
+    }
+    return path
+  },
+  getProfiles (state) {
+    return state.profiles
   }
 }
 
@@ -53,7 +168,16 @@ const mutations = {
   },
   'UPDATE_WIDTH' (state, payload) {
     state.singleWindow.sizes.width = payload
+  },
+  'UPDATE_WINDOW_TYPE' (state, payload) {
+    state.checked.window = payload
+  },
+  'UPDATE_WINDOW_PROFILE' (state, payload) {
+    state.checked.profile = payload
   }
+  // 'TOGGLE_SHOW_PROFILE' (state) {
+  //   state.profiles.show = !state.profiles.show
+  // }
 }
 
 const actions = {
@@ -62,6 +186,12 @@ const actions = {
   },
   updateWidth ({ commit }, payload) {
     commit('UPDATE_WIDTH', payload)
+  },
+  updateWindowType ({ commit }, payload) {
+    commit('UPDATE_WINDOW_TYPE', payload)
+  },
+  updateWindowProfile ({ commit }, payload) {
+    commit('UPDATE_WINDOW_PROFILE', payload)
   }
 }
 
