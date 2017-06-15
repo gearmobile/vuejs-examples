e335165de75a0b4d76bc3894cdd437eca3972376
gearmobile <gearmobile@gmail.com>
2017-06-15 11:15:35 +0300

    add app-cleaner

---
+++ app-cleaner/src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #app.container
+    .row
+      .col-md-10.col-md-offset-1
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin-top: 60px;
+  }
+</style>
---
+++ app-cleaner/src/components/primo.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  #primo
+</template>
+
+<script>
+export default {
+  name: 'primo'
+}
+</script>
+
+<style scoped>
+  /**/
+</style>
