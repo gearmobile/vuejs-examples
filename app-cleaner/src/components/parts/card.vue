12c11784f4a53714cd378c8a95b7f43c7a209f31
gearmobile <gearmobile@gmail.com>
2017-06-17 11:34:18 +0300

    cleaner

--- app-cleaner/src/components/parts/card.vue
+++ app-cleaner/src/components/parts/card.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   .card.col-md-3
-    .well.text-center
+    .well.card__wrapper
       h6 {{ card.title }}
       .card__item
         button.btn.btn-default( type="button" ) -
         input.form-control.text-center( type="text", v-model="card.value" )
-        button.btn.btn-default( type="button" ) +
+        button.btn.btn-default( type="button", @click="addOrder()" ) +
 </template>
 
 <script>
@@ -21,14 +21,40 @@
     },
     methods: {
       ...mapActions({
-        //
-      })
+        orderAdd: 'addOrder',
+        orderDelete: 'deleteOrder'
+      }),
+      addOrder () {
+        const order = {
+          name: this.card.name,
+          price: this.card.price,
+          value: this.card.value,
+          time: this.card.time,
+          title: this.card.title
+        }
+        this.orderAdd(order)
+      },
+      removeOrder () {
+        const order = {
+          name: this.card.name,
+          price: this.card.price,
+          value: this.card.value,
+          time: this.card.time,
+          title: this.card.title
+        }
+        this.orderDelete(order)
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
   .card {
+    text-align: center;
+    
+    &__wrapper {
+      min-height: 160px;
+    }
 
     &__item {
       display: flex;
