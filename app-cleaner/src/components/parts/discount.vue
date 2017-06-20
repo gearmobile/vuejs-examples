1f92bdf8e0ad58d1e555002f1a6f1a40072b5cff
gearmobile <gearmobile@gmail.com>
2017-06-20 21:46:23 +0300

    cleaner

---
+++ app-cleaner/src/components/parts/discount.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .col-md-4
+    .text-center.well.main__thumb
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app-cleaner/src/data/discount.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: 'first',
+    title: '1 раз в месяц',
+    value: 10,
+    state: false
+  },
+  {
+    name: 'second',
+    title: '1 раз в 2 недели',
+    value: 15,
+    state: false
+  },
+  {
+    name: 'third',
+    title: '1 раз в неделю',
+    value: 20,
+    state: false
+  }
+]
