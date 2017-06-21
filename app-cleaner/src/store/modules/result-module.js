715aeac272251a9b2b5da23dd5176e12232e720c
gearmobile <gearmobile@gmail.com>
2017-06-21 14:52:59 +0300

    cleaner

--- app-cleaner/src/store/modules/result-module.js
+++ app-cleaner/src/store/modules/result-module.js
@@ -36,10 +36,9 @@ const mutations = {
   },
   'SET_PROMO_VALUE' (state, payload) {
     state.promocode.value = payload
-  },
-  'CHECK_PROMO' (state) {
     if (parseInt(state.promocode.value) === state.promocode.check) {
       state.promocode.status = true
+      state.promocode.error = false
     } else {
       state.promocode.error = true
     }
