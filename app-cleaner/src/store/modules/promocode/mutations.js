94cfc5a3e34939a31f8c2a2f4e8c722aefa57b06
gearmobile <gearmobile@gmail.com>
2017-06-24 15:27:54 +0300

    cleaner - check promocode component

--- app-cleaner/src/store/modules/promocode/mutations.js
+++ app-cleaner/src/store/modules/promocode/mutations.js
@@ -3,8 +3,7 @@ export default {
     state.promocode = payload
   },
   'SET_PROMO_VALUE' (state, payload) {
-    state.promocode.value = payload
-    if (parseInt(state.promocode.value) === state.promocode.check) {
+    if (parseInt(payload) === state.promocode.check) {
       state.promocode.status = true
       state.promocode.error = false
     } else {
