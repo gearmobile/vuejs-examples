669a33133511cf1c9bddfb4b0c50b3cf5d9e0d15
gearmobile <gearmobile@gmail.com>
2017-06-28 21:33:34 +0300

    foto-app

--- foto-app/src/components/navigation.vue
+++ foto-app/src/components/navigation.vue
@@ -1,22 +1,22 @@
 <template lang="pug">
-  ul.nav.nav-justified
+  ul.nav.nav-justified.nav-pills
     router-link( tag="li", to="/", role="presentation", active-class="active", exact )
-      a.text-uppercase( href="#" )
+      a( href="#" )
         | step 1
     router-link( tag="li", to="/secondo", role="presentation", active-class="active" )
-      a.text-uppercase( href="#" )
+      a( href="#" )
         | step 2
     router-link( tag="li", to="/tetro", role="presentation", active-class="active" )
-      a.text-uppercase( href="#" )
+      a( href="#" )
         | step 3
     router-link( tag="li", to="/quattro", role="presentation", active-class="active" )
-      a.text-uppercase( href="#" )
+      a( href="#" )
         | step 4
     router-link( tag="li", to="/cinque", role="presentation", active-class="active" )
-      a.text-uppercase( href="#" )
+      a( href="#" )
         | step 5
     router-link( tag="li", to="/sei", role="presentation", active-class="active" )
-      a.text-uppercase( href="#" )
+      a( href="#" )
         | step 6
 </template>
 
@@ -27,6 +27,17 @@
 </script>
 
 <style lang="stylus" scoped>
+
   .nav
     margin-bottom .6rem
+
+    & > li > a
+      text-transform uppercase
+      color #000
+      font-weight 700
+
+    & > li.active > a
+      background-color #f6e95f
+      color #000
+
 </style>
