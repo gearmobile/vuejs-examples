685ac9fce9aab3e89fa28a4ec2796aa9c0de277a
gearmobile <gearmobile@gmail.com>
2017-07-10 19:28:19 +0300

    photo-app

--- photo-app/src/components/four.vue
+++ photo-app/src/components/four.vue
@@ -1,7 +1,14 @@
 <template lang="pug">
-  v-card
-    v-card-text
-      | {{msg}}
+  v-container
+    v-layout( row, wrap )
+      div.text-xs-center.yellow.py-3.mb-1( style="width: 100%" )
+        h2.display-1.mb-0
+          | {{ title }}
+        h3.title.mb-0
+          | {{ subtitle }}
+      v-flex( xs12 )
+        v-text-field( name="greeting", label="Ваше поздравление", v-model="greeting", multi-line )
+
 </template>
 
 <script>
@@ -9,7 +16,9 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'four'
+      title: 'индивидуальное поздравление',
+      subtitle: '(мы обязательно напишем его на сертификате)',
+      greeting: null
     }
   }
 }
--- photo-app/src/components/six.vue
+++ photo-app/src/components/six.vue
@@ -1,7 +1,27 @@
 <template lang="pug">
-  v-card
-    v-card-text
-      | {{msg}}
+  v-container( fluid )
+    div.text-xs-center.yellow.py-3.mb-1( style="width: 100%" )
+      h2.display-1.mb-0
+        | {{ title }}
+    v-layout( row, wrap )
+      v-flex( xs4 )
+        v-subheader
+          | ФИО
+      v-flex( xs8 )
+        v-text-field( name="name", label="name", required )
+    v-layout( row, wrap )
+      v-flex( xs4 )
+        v-subheader
+          | Номер телефона
+      v-flex( xs8 )
+        v-text-field( name="phone", label="phone", required )
+    v-layout( row, wrap )
+      v-flex( xs4 )
+        v-subheader
+          | E-mail
+      v-flex( xs8 )
+        v-text-field( name="email", label="email", required )
+
 </template>
 
 <script>
@@ -9,7 +29,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'six'
+      title: 'оформление заказа'
     }
   }
 }
