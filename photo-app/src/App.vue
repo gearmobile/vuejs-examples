3ed37a678991cf39798ec9af54879e5312b08925
gearmobile <gearmobile@gmail.com>
2017-07-10 15:57:47 +0300

    photo-app - try implement vuetifyjs; no result

--- photo-app/src/App.vue
+++ photo-app/src/App.vue
@@ -27,12 +27,18 @@
             | step 1
           v-btn.primary( @click.native="stage = 2" )
             | next
-      v-stepper-content( step="2" )
-        v-card
-          v-card-text( height="200px" )
-            | step 2
-          v-btn.primary( @click.native="stage = 3" )
-            | next
+      v-layout( row, wrap )
+        v-flex( xs-8 )
+          v-stepper-content( step="2" )
+            v-card
+              v-card-text( height="200px" )
+                | step 2
+              v-btn.primary( @click.native="stage = 3" )
+                | next
+        v-flex( xs-4 )
+          v-card
+            v-card-text
+              | content
       v-stepper-content( step="3" )
         v-card
           v-card-text( height="200px" )
@@ -58,6 +64,49 @@
           v-btn.primary( @click.native="stage = 1" )
             | next
 
+    v-tabs( v-model="active" )
+      v-tabs-bar
+        v-tabs-item( href="#step1", ripple )
+          | step 1
+        v-tabs-item( href="#step2", ripple )
+          | step 2
+        v-tabs-item( href="#step3", ripple )
+          | step 3
+        v-tabs-item( href="#step4", ripple )
+          | step 4
+        v-tabs-item( href="#step5", ripple )
+          | step 5
+        v-tabs-item( href="#step6", ripple )
+          | step 6
+        v-tabs-slider.white
+      v-tabs-content( id="step1" )
+        v-card( flat )
+          v-card-text
+            | step 1
+      v-tabs-content( id="step2" )
+        v-card( flat )
+          v-card-text
+            | step 2
+      v-tabs-content( id="step3" )
+        v-card( flat )
+          v-card-text
+            | step 3
+      v-tabs-content( id="step4" )
+        v-card( flat )
+          v-card-text
+            | step 4
+      v-tabs-content( id="step5" )
+        v-card( flat )
+          v-card-text
+            | step 5
+      v-tabs-content( id="step6" )
+        v-card( flat )
+          v-card-text
+            | step 6
+
+    v-btn.primary( @click.native="nextStep" )
+      | next step
+
 
     v-footer( :fixed="fixed" )
       span
@@ -70,7 +119,17 @@
     data () {
       return {
         fixed: true,
-        stage: 0
+        stage: 0,
+        steps: ['step-1', 'step-2', 'step-3', 'step-4', 'step-5', 'step-6'],
+        text: 'lorem ipsum dolor est',
+        active: null
+      }
+    },
+    methods: {
+      nextStep () {
+        // const index = ((this.steps.indexOf(this.steps.find(el => el.title === this.active)) + 1)) % this.steps.length
+        // this.active = this.steps[index].title
+        this.active = this.steps[(this.steps.indexOf(this.active) + 1) % this.steps.length]
       }
     },
     computed: {
