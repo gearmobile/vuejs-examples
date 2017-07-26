520087106ec18dde4554d84abbe582b4558af4d1
Valery Semenencko <gearmobile@gmail.com>
2017-07-26 15:59:40 +0300

    meetup - add firebase support

---
+++ meetup-app/src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyDQXaORNY_j1FKihu3WSzseKXjaGT5Y3G0',
+  authDomain: 'meetup-app-530c6.firebaseapp.com',
+  databaseURL: 'https://meetup-app-530c6.firebaseio.com',
+  projectId: 'meetup-app-530c6',
+  storageBucket: '',
+  messagingSenderId: '901248342813'
+}
+
+firebase.initializeApp(config)
