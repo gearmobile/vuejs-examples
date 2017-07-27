ad6a8b7ff3e7d018e1f9bcc4e92df38bf6b6c6e7
Valery Semenencko <gearmobile@gmail.com>
2017-07-27 13:02:24 +0300

    meetup - add router links toolbar items

--- meetup-app/src/App.vue
+++ meetup-app/src/App.vue
@@ -4,9 +4,9 @@
   v-app
 
     // SIDEBAR PANEL
-    v-navigation-drawer( v-model="showSide" )
+    v-navigation-drawer( v-model="showSide", temporary )
       v-list
-        v-list-tile( v-for="item in items", :key="item.title" )
+        v-list-tile( v-for="item in items", :key="item.title", router, :to="item.link" )
           v-list-tile-action
             v-icon
               | {{ item.icon }}
@@ -14,10 +14,11 @@
             | {{ item.title }}
 
     // TOOLBAR
-    v-toolbar
+    v-toolbar.teal( dark )
       v-toolbar-side-icon.hidden-sm-and-up( @click.native.stop="showSide = !showSide" )
       v-toolbar-title
-        | MeetupApp
+        router-link( tag="span", to="/", style="cursor: pointer" )
+          | MeetupApp
       v-spacer
       v-toolbar-items.hidden-xs-only
         v-btn( flat, v-for="item in items", :key="item.title" )
--- meetup-app/src/components/user/profile.vue
+++ meetup-app/src/components/user/profile.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-  //
+  .profile
+    | profile
 </template>
 
 <script>
