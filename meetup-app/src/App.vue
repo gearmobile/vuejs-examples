f93c48c2a17b01f4d51949ba2567d4b882668da5
Valery Semenencko <gearmobile@gmail.com>
2017-07-27 14:52:30 +0300

    meetup - fix some updates in v.0.14 vuetify

--- meetup-app/src/App.vue
+++ meetup-app/src/App.vue
@@ -6,7 +6,7 @@
     // SIDEBAR PANEL
     v-navigation-drawer( v-model="showSide", temporary )
       v-list
-        v-list-tile( v-for="item in items", :key="item.title", router, :to="item.link" )
+        v-list-tile( v-for="item in items", :key="item.title", :to="item.link" )
           v-list-tile-action
             v-icon
               | {{ item.icon }}
@@ -15,13 +15,13 @@
 
     // TOOLBAR
     v-toolbar.teal( dark )
-      v-toolbar-side-icon.hidden-sm-and-up( @click.native.stop="showSide = !showSide" )
+      v-toolbar-side-icon.hidden-sm-and-up( @click.stop="showSide = !showSide" )
       v-toolbar-title
         router-link( tag="span", to="/", style="cursor: pointer" )
           | MeetupApp
       v-spacer
       v-toolbar-items.hidden-xs-only
-        v-btn( flat, v-for="item in items", :key="item.title", router, :to="item.link" )
+        v-btn( flat, v-for="item in items", :key="item.title", :to="item.link" )
           v-icon( left, dark )
             | {{ item.icon }}
           | {{ item.title }}
