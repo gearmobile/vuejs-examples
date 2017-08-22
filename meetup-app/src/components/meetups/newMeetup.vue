1b79c24c4ada2422f199bdc2fcedabfd903982e7
gearmobile <gearmobile@gmail.com>
2017-08-22 15:37:38 +0300

    meetup-app - gen ids

--- meetup-app/src/components/meetups/newMeetup.vue
+++ meetup-app/src/components/meetups/newMeetup.vue
@@ -13,38 +13,80 @@
           // NAME FIELD
           v-layout( row )
             v-flex( xs12, sm6, offset-sm3 )
-              v-text-field( name="name", id="name", label="Name", required )
+              v-text-field( name="name", id="name", label="Name", v-model="meetup.name", required )
 
           // LOCATION FIELD
           v-layout( row )
             v-flex( xs12, sm6, offset-sm3 )
-              v-text-field( name="location", id="location", label="Location", required )
+              v-text-field( name="location", id="location", label="Location", v-model="meetup.location", required )
 
           // IMAGE FIELD
           v-layout( row )
             v-flex( xs12, sm6, offset-sm3 )
-              v-text-field( name="image", id="image", label="Image", required )
+              v-text-field( name="image", id="image", label="Image", v-model="meetup.image", required )
 
           // IMAGE PREVIEW
           v-layout( row )
             v-flex( xs12, sm6, offset-sm3 )
-              img( src="http://via.placeholder.com/800x400", alt="Image", style="width: 100%; height: auto" )
+              img( :src="meetup.image", :alt="meetup.name", style="width: 100%; height: auto" )
 
           // DESCRIPTION FIELD
           v-layout( row )
             v-flex( xs12, sm6, offset-sm3 )
-              v-text-field( name="description", id="description", label="Description", required, multi-line )
+              v-text-field( name="description", id="description", label="Description", v-model="meetup.description", required, multi-line )
 
-          // NAME FIELD
+          //- // DATE FIELD
+          //- v-layout( row )
+          //-   v-flex( xs12, sm6, offset-sm3 )
+          //-     //
+
+          // DATE FIELD
+          v-layout.mb-2( row )
+            v-flex( xs12, sm6, offset-sm2 )
+              v-date-picker( v-model="meetup.date" )
+            
+          // TIME FIELD
+          v-layout.mb-2( row )
+            v-flex( xs12, sm6, offset-sm2 )
+              v-time-picker( v-model="meetup.time", format="24hr" )
+
+          // SUBMIT FIELD
           v-layout( row )
             v-flex( xs12, sm6, offset-sm3 )
-              v-text-field( name="name", id="name", label="Name", required )
+              v-btn.primary( :disabled="!show", type="submit" )
+                | create meetup
 
 </template>
 
 <script>
+  import { mapActions } from 'vuex'
+
   export default {
-    //
+    data () {
+      return {
+        meetup: {
+          name: null,
+          location: null,
+          image: 'http://via.placeholder.com/800x400',
+          description: null,
+          date: null,
+          time: null
+        }
+      }
+    },
+    computed: {
+      show () {
+        return this.meetup.name !== null && this.meetup.location !== null && this.meetup.image !== null && this.meetup.description !== null && this.meetup.date !== null && this.meetup.time !== null
+      }
+    },
+    methods: {
+      ...mapActions({
+        createMeetup: 'newMeetup'
+      }),
+      onSend () {
+        this.createMeetup(this.meetup)
+      }
+    }
   }
 </script>
 
--- meetup-app/src/store/index.js
+++ meetup-app/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import _ from 'lodash'
 
 Vue.use(Vuex)
 
@@ -26,15 +27,31 @@ const state = {
       date: '03-March-2017',
       description: 'Lorem ipsum dolor sit amet, ei hendrerit constituto dissentias vim. Usu doctus facilisi torquatos cu, mel eligendi pericula eu. Quod nulla omnes ius ea, an nam sapientem persecuti disputationi, at qui partem expetendis disputando. Te volumus prodesset nam. Et sit nibh choro dicunt, an idque dicunt minimum nec. Viris possim verear ne mea.'
     }
+  ],
+  users: [
+    //
   ]
 }
 
 const mutations = {
-  //
+  'NEW_MEETUP' (state, payload) {
+    state.meetups.push(payload)
+  }
 }
 
 const actions = {
-  //
+  newMeetup ({ commit }, payload) {
+    const meetup = {
+      id: _.toString(_.toNumber(_.max(_.map(state.meetups, 'id'))) + 1),
+      title: payload.name,
+      location: payload.location,
+      path: payload.image,
+      description: payload.description,
+      date: payload.date,
+      time: payload.time
+    }
+    commit('NEW_MEETUP', meetup)
+  }
 }
 
 const getters = {
