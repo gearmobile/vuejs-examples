ad6d4bc2b05202b5d9adaad59b73239c01e6e7b6
gearmobile <gearmobile@gmail.com>
2017-08-24 22:04:48 +0300

    meetup-app - signup page

--- meetup-app/src/components/users/signUp.vue
+++ meetup-app/src/components/users/signUp.vue
@@ -1,10 +1,53 @@
 <template lang="pug">
-  //
+  
+  v-container
+    v-layout( row )
+      v-flex( xs12, sm6, offset-sm3 )
+        v-card
+          v-card-text
+            v-container
+              // FORM
+              form( @submit.prevent='onSubmit()' )
+                // EMAIL
+                v-layout( row )
+                  v-flex( xs12 )
+                    v-text-field( name='email', id='email', label='Email', type='email', v-model='signup.email', required )
+                // PASSWORD
+                v-layout( row )
+                  v-flex( xs12 )
+                    v-text-field( name='password', id='password', label='Password', type='password', v-model='signup.password', required )
+                // CONFIRM PASSWORD
+                v-layout( row )
+                  v-flex( xs12 )
+                    v-text-field( name='confirmPassword', id='confirmPassword', label='Confirm Password', type='password', v-model='signup.confirm', :rules='[comparePassword]' )
+                // SIGN UP
+                v-layout( row )
+                  v-flex( xs12 )
+                    v-btn.primary( type='submit' )
+                      | sign up
 </template>
 
 <script>
   export default {
-    //
+    data () {
+      return {
+        signup: {
+          email: null,
+          password: null,
+          confirm: null
+        }
+      }
+    },
+    computed: {
+      comparePassword () {
+        return this.signup.password !== this.signup.confirm ? 'Passwords do not match' : ''
+      }
+    },
+    methods: {
+      onSubmit () {
+        //
+      }
+    }
   }
 </script>
 
--- meetup-app/src/main.js
+++ meetup-app/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -14,5 +15,14 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: {App}
+  components: {App},
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDQXaORNY_j1FKihu3WSzseKXjaGT5Y3G0',
+      authDomain: 'meetup-app-530c6.firebaseapp.com',
+      databaseURL: 'https://meetup-app-530c6.firebaseio.com',
+      projectId: 'meetup-app-530c6',
+      storageBucket: 'meetup-app-530c6.appspot.com'
+    })
+  }
 }).$mount('#app')
